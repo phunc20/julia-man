7dc51137cb55ffe41d06b5041363163878e3c904
phunc20 <wucf20@gmail.com>
2021-03-11 17:44:31 +0700

    more reading on Flux/Basics

--- Flux/basics/grad.jl
+++ Flux/basics/grad.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.21
+# v0.12.20
 
 using Markdown
 using InteractiveUtils
@@ -7,16 +7,16 @@ using InteractiveUtils
 # ╔═╡ 8ce76b60-8197-11eb-2ba0-1fbd56efda05
 using Flux
 
-# ╔═╡ 87e0fa16-8195-11eb-1390-1740676ce3a0
-path_tmp = mktempdir()
-
-# ╔═╡ db37122a-8197-11eb-1cbf-29fc1bcadf1f
-typeof(path_tmp)
+# ╔═╡ d10152c2-824e-11eb-13f6-6baeb4aec189
+begin
+  using Random
+  Random.seed!(42)
+end
 
 # ╔═╡ 8d070ed4-8197-11eb-1ca9-f5354be2ffbd
 begin
   import Pkg
-  Pkg.activate(path_tmp)
+  Pkg.activate(mktempdir())
   Pkg.add([
     "Flux"
   ])
@@ -146,12 +146,166 @@ KeyError: key [1, 2, 3] not found
 
 # ╔═╡ 2262e6d8-81bb-11eb-3970-ddee38c19cc4
 md"""
-## Simple Models
+## A Simple Linear Regression Model
 """
 
+# ╔═╡ d1008464-824e-11eb-08d2-bb62d6629dc3
+W = rand(2, 5)
+
+# ╔═╡ d0ffcf60-824e-11eb-2608-f57deece6f4a
+b = rand(2)
+
+# ╔═╡ cfbd8408-824e-11eb-1dc8-1b6d1d223b96
+predict(x) = W*x .+ b  # x should be in R⁵
+
+# ╔═╡ 6c77eb26-824f-11eb-0d19-e164e5f38851
+function loss(x, y)
+  ŷ = predict(x)
+  sum((y .- ŷ).^2)
+end
+
+# ╔═╡ 6c397e2c-824f-11eb-2866-79a94e1bce3a
+md"""
+**Note**. `ŷ` can be obtained by typing `y\hat` plus `Tab`.$(HTML("<br>"))
+Not the other way around: `\hat` plus `Tab` plus `y` gives ` ̂y`, which is not recognizable in Julia/Pluto.
+
+The same logic applies to `bar` as well: `W̄` is
+- constructed by the combination `W\bar` plus `Tab`
+- not by `\bar` plus `Tab` plus `W`
+"""
+
+# ╔═╡ 6bc0b0e6-824f-11eb-1330-31c0422a6f1c
+x0, y0 = rand(5), rand(2)  # data
+
+# ╔═╡ 6b36ebfe-824f-11eb-3f16-ffd4687d05b6
+loss(x0, y0)
+
+# ╔═╡ a4bfbb96-824f-11eb-0f40-9daf904940c4
+grad = gradient(() -> loss(x0, y0), params(W, b))
+
+# ╔═╡ f3e6f3c2-824f-11eb-204b-6997de39bb92
+begin
+  W .-= 0.1 .* grad[W]
+  b .-= 0.1 .* grad[b]
+end
+
+# ╔═╡ a43348f0-824f-11eb-2384-db33794105c6
+loss(x0, y0)  # with the updated W and b, loss indeed decreases
+
+# ╔═╡ b2c5e90a-8251-11eb-141d-b309e053bd5e
+md"""
+## Building Layers
+"""
+
+# ╔═╡ 6fa6f0da-8252-11eb-190b-836cd888b111
+md"""
+There exist multiple ways to build Neural Network Layers. We shall introduce a few of them, with increasing level of manual work.
+
+### Manually1
+"""
+
+# ╔═╡ b2a7dc28-8251-11eb-2523-21048c2ca80b
+begin
+  W1 = rand(3,5)
+  b1 = rand(3)
+  layer1(x) = W1*x .+ b1
+end
+
+# ╔═╡ b28be59a-8251-11eb-3a91-3996e3d04040
+begin
+  W2 = rand(2,3)
+  b2 = rand(2)
+  layer2(x) = W2*x .+ b2
+end
+
+# ╔═╡ b26c495e-8251-11eb-1d36-a31212b92f01
+begin
+  model_manual1(x) = layer2(σ.(layer1(x)))
+  model_manual1(rand(5))
+end
+
+# ╔═╡ b24a0dda-8251-11eb-1904-23fcfbed48fe
+md"""
+### Manually2: Template
+"""
+
+# ╔═╡ b229ae00-8251-11eb-35c0-a3709955aa50
+function linear(n_in, n_out)
+  W = randn(n_out, n_in)  # we could have used rand() instead. It's ok, too.
+  b = randn(n_out)
+  x -> W*x .+ b
+end
+
+# ╔═╡ 0435d5ba-8253-11eb-1b84-a7c2f752aa4c
+begin
+  linear1 = linear(5, 3)  # we can access linear1.W, etc.
+  linear2 = linear(3, 2)
+end
+
+# ╔═╡ 04246974-8253-11eb-1415-cfd21ba19213
+linear1.W
+
+# ╔═╡ 03f7e1b0-8253-11eb-1acf-4fbfa23b2d98
+linear2.b
+
+# ╔═╡ 03aefc98-8253-11eb-1b8a-c3a3d9d642bf
+md"""
+This is a rather special feature of Julia: _Functions can have attributes_.
+"""
+
+# ╔═╡ b1d52470-8251-11eb-231c-b3287d3af027
+model_manual2(x) = linear2(σ.(linear1(x)))
+
+# ╔═╡ b161cf5e-8251-11eb-2885-99e9b56e6d9f
+model_manual2(rand(5))
+
+# ╔═╡ 049d3084-8253-11eb-0324-b5f563aff8f3
+md"""
+### Manually3: Template
+"""
+
+# ╔═╡ 049570d0-8253-11eb-2d1d-35cacf8c0597
+begin
+  struct Affine
+    W
+    b
+  end
+  
+  Affine(n_in::Integer, n_out::Integer) =
+    Affine(randn(n_out, n_in), randn(n_out))
+end
+
+# ╔═╡ e81a6060-8254-11eb-3608-25569cd3a40f
+md"""
+**Note.** In Pluto, the two definitions above **_must be grouped into a single cell_**; otherwise, Pluto will
+complain about multiple definitions of the same object/data structure.
+"""
+
+# ╔═╡ b59da87c-8254-11eb-0823-3f1bedd509e0
+# Overload call, so an instance of the object can be used as a function
+(m::Affine)(x) = m.W * x .+ m.b
+
+# ╔═╡ b565c036-8254-11eb-2472-758e00842f4b
+a = Affine(10,5)
+
+# ╔═╡ 046fb640-8253-11eb-18fa-b34bef08ad65
+a(rand(10))
+
+# ╔═╡ 5ed138fe-8256-11eb-39f1-371d2390dc3b
+md"""
+`Flux` provides a `Dense` object which does more or less the same thing as above. `Dense` can be specified
+- w/ an activation function
+- w/o an activatoin function
+Just as the following two cells show.
+"""
+
+# ╔═╡ 49799d40-8256-11eb-2f49-4d3e6ca8e337
+Dense(10, 5, σ)(rand(10))
+
+# ╔═╡ 492a1200-8256-11eb-25ea-2fd41050493f
+Dense(10, 5)(rand(10))
+
 # ╔═╡ Cell order:
-# ╠═87e0fa16-8195-11eb-1390-1740676ce3a0
-# ╠═db37122a-8197-11eb-1cbf-29fc1bcadf1f
 # ╠═8d070ed4-8197-11eb-1ca9-f5354be2ffbd
 # ╠═8ce76b60-8197-11eb-2ba0-1fbd56efda05
 # ╟─d2130040-81b8-11eb-1475-0b09814b3f1b
@@ -177,3 +331,36 @@ md"""
 # ╠═d8f58d02-81ba-11eb-026b-ed82844e7051
 # ╟─ef711a12-819d-11eb-074d-897cd7b3f324
 # ╟─2262e6d8-81bb-11eb-3970-ddee38c19cc4
+# ╠═d10152c2-824e-11eb-13f6-6baeb4aec189
+# ╠═d1008464-824e-11eb-08d2-bb62d6629dc3
+# ╠═d0ffcf60-824e-11eb-2608-f57deece6f4a
+# ╠═cfbd8408-824e-11eb-1dc8-1b6d1d223b96
+# ╠═6c77eb26-824f-11eb-0d19-e164e5f38851
+# ╟─6c397e2c-824f-11eb-2866-79a94e1bce3a
+# ╠═6bc0b0e6-824f-11eb-1330-31c0422a6f1c
+# ╠═6b36ebfe-824f-11eb-3f16-ffd4687d05b6
+# ╠═a4bfbb96-824f-11eb-0f40-9daf904940c4
+# ╠═f3e6f3c2-824f-11eb-204b-6997de39bb92
+# ╠═a43348f0-824f-11eb-2384-db33794105c6
+# ╟─b2c5e90a-8251-11eb-141d-b309e053bd5e
+# ╟─6fa6f0da-8252-11eb-190b-836cd888b111
+# ╠═b2a7dc28-8251-11eb-2523-21048c2ca80b
+# ╠═b28be59a-8251-11eb-3a91-3996e3d04040
+# ╠═b26c495e-8251-11eb-1d36-a31212b92f01
+# ╟─b24a0dda-8251-11eb-1904-23fcfbed48fe
+# ╠═b229ae00-8251-11eb-35c0-a3709955aa50
+# ╠═0435d5ba-8253-11eb-1b84-a7c2f752aa4c
+# ╠═04246974-8253-11eb-1415-cfd21ba19213
+# ╠═03f7e1b0-8253-11eb-1acf-4fbfa23b2d98
+# ╟─03aefc98-8253-11eb-1b8a-c3a3d9d642bf
+# ╠═b1d52470-8251-11eb-231c-b3287d3af027
+# ╠═b161cf5e-8251-11eb-2885-99e9b56e6d9f
+# ╟─049d3084-8253-11eb-0324-b5f563aff8f3
+# ╠═049570d0-8253-11eb-2d1d-35cacf8c0597
+# ╟─e81a6060-8254-11eb-3608-25569cd3a40f
+# ╠═b59da87c-8254-11eb-0823-3f1bedd509e0
+# ╠═b565c036-8254-11eb-2472-758e00842f4b
+# ╠═046fb640-8253-11eb-18fa-b34bef08ad65
+# ╟─5ed138fe-8256-11eb-39f1-371d2390dc3b
+# ╠═49799d40-8256-11eb-2f49-4d3e6ca8e337
+# ╠═492a1200-8256-11eb-25ea-2fd41050493f
