59b23b2d6029366e9f675c84f002b1628a3ff31d
phunc20 <wucf20@gmail.com>
2022-05-18 23:55:19 +0700

    calculate prime numbers to simulate processing time: NOT YET finished

--- async/experiment/02/utils.jl
+++ async/experiment/02/utils.jl
@@ -43,67 +43,35 @@ function get_prime!(n)
   return primes[n]
 end
 
-function write_simple_json(path::String, c::Char)
-  dict = Dict(c => Int(c) - Int('a') + 1)
+function write_prime_json(path::String, n::Integer)
+  dict = Dict(k => get_prime!(k) for k in 1:n)
   data = JSON.json(dict)
   open(path, "w") do io
     write(io, data)
   end
 end
 
-function write_prime_json(path::String, c::Char)
-  dict = Dict(c => Int(c) - Int('a') + 1)
-  data = JSON.json(dict)
-  open(path, "w") do io
-    write(io, data)
-  end
-end
-
-function bad_async_gen_data()
+function async_gen_data()
   folder = mktempdir()
   function producer(channel::Channel)
-    for c in 'a':'z'
-      put!(channel, c)
+    start = 1000
+    step = start
+    stop = 10_000
+    for n in start:step:stop
+      put!(channel, n)
     end
   end
-  for c in Channel(producer)
-    @async write_simple_json("$folder/$c.json", c)
-  end
-end
-
-function wrong_async_gen_data()
-  folder = mktempdir()
-  for c in 'a':'z'
-    path = "$folder/$c.json"
-    @async write_simple_json(path, c)
+  for n in Channel(producer)
+    @async write_prime_json("$folder/$n.json", n)
   end
 end
 
 function seq_gen_data()
   folder = mktempdir()
-  for c in 'a':'z'
-    path = "$folder/$c.json"
-    if !isfile(path)
-      dict = Dict(c => Int(c) - Int('a') + 1)
-      data = JSON.json(dict)
-      open(path, "w") do io
-        write(io, data)
-      end
-    end
+  start = 1000
+  step = start
+  stop = 10_000
+  for n in start:step:stop
+    write_prime_json("$folder/$n.json", n)
   end
 end
-
-function get_content(json_path)
-  content = open(json_path, "r") do io
-    raw_content = read(io, String)
-    JSON.parse(raw_content)
-  end
-  return content
-end
-
-function seq_merge(json_paths)
-end
-
-function async_merge(json_paths)
-end
-
